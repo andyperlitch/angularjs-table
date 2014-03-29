deccd028baafb751fb48ff5787adc52f3ae70638
Andy Perlitch <andyperlitch@gmail.com>
2014-03-29 07:49:26 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngTabledApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
