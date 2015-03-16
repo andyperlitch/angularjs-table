c333c48cee0aab076354bc0ebee82237f175ca1e
Jessica Wang <jessica@datatorrent.com>
2015-03-16 16:49:45 -0700

    SPOI-4234: dist file change by grunt build

--- dist/mlhr-table.js
+++ dist/mlhr-table.js
@@ -613,7 +613,7 @@ angular.module('datatorrent.mlhrTable.directives.mlhrTableCell', ['datatorrent.m
         cellMarkup = '{{ row[column.key] | ' + column.ngFilter + ':row }}';
       } else if (column.format) {
         cellMarkup = '{{ column.format(row[column.key], row, column) }}';
-      } else if ('getter' in scope.options && typeof scope.options.getter === 'function') {
+      } else if ({}.hasOwnProperty.call(scope.options, 'getter') && typeof scope.options.getter === 'function') {
         cellMarkup = '{{ options.getter(column.key, row) }}';
       } else {
         cellMarkup = '{{ row[column.key] }}';
