d056ad18f7d562a2a7e36b84a9325f3e1a75882e
Andrew Perlitch <perlitch@cisco.com>
2017-01-25 11:04:56 -0800

    added update gh-pages script

---
+++ update-gh-pages.sh
@@ -0,0 +1,28 @@
+# Updates the github pages
+
+
+# Reset the branch
+git checkout master
+git branch -D gh-pages
+git checkout -b gh-pages
+
+# Link demo files and create templates file
+for f in $(ls app); do ln -sf app/$f; done
+rm -f app/scripts/templates.js
+grunt html2js:dist
+mv dist/templates.js app/scripts/templates.js
+
+# Install bower stuff
+bower install
+ln -sf app/bower_components
+
+# Stage and commit
+git add app/bower_components -f
+git add -A
+COMMIT_ID=$(git rev-parse HEAD)
+git commit -m "gh-pages update for commit: ${COMMIT_ID}"
+
+# Push to remote
+git push --set-upstream origin gh-pages -f
+git checkout master
+git clean -f
